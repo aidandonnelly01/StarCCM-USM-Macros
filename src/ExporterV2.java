60816ff8dbb4a695ceba4ff9b13c72fdc28a2280
Aidan Donnelly SE2020 <aidan.donnelly.2020@uni.strath.ac.uk>
2022-10-25 14:03:35 +0100

    added comments to exporterV2

--- src/ExporterV2.java
+++ src/ExporterV2.java
@@ -1,27 +1,40 @@
+//StarCCM exporter macro 
+//V2.0
+
+//import statements required for java stuff
 import java.util.*;
 import java.io.File;
 
-
+//import statements required for star stuff
 import star.common.*;
 import star.base.neo.*;
 import star.vis.*;
 import star.screenplay.*;
 
+//Class name is exporter inheriting methods from StarMacro 
 public class ExporterV2 extends StarMacro{
 
     //Star searches for and runs the 'execute' method, all methods to be run should be in here
     public void execute() {
+		//Creates simulation object named simulation_0 
         Simulation simulation_0 = getActiveSimulation();
+		//sep contains the standard filepath separator for the users operating 
+		//system, for windows this is '\'
         String sep = System.getProperty("file.separator");
+		//dir stores the directory where the sim file is contained
         File dir = new File(simulation_0.getSessionDir() + sep + simulation_0.getPresentationName());
 
+		//attempts to create another directory in the sim file's directory named after the sim file
+		//mkdir will return false if the directory already exists to prevent data deletion 
         if(dir.mkdir()) {
+			//creates directory and exports scenes
             simulation_0.println("Simulation Directory created: " + dir.getAbsolutePath());
-            //getScenes(simulation_0, dir, sep);
+            getScenes(simulation_0, dir, sep);
             getPlots(simulation_0, dir, sep);
         }
         else {
-            simulation_0.println("Simulation directory failed to create");
+			//if the directory cannot be created then tell the user, ends macro
+            simulation_0.println("Simulation directory failed to create, it may already exist");
         }
     }
 
@@ -35,9 +48,8 @@ public class ExporterV2 extends StarMacro{
         String sep = fileSeparator;
         File dir = new File(simulationDirectory + sep + "Scenes");
 
-
        if (dir.mkdir()) {
-            //iterate through every scene and export the scenes into a .jpg file
+            //iterate through every scene and export the scenes into a .jpg file and a 3D representation 
             for (Scene scene : simulation_0.getSceneManager().getScenes()) {
                 scene.printAndWait(resolvePath(dir + sep + scene.getPresentationName() + " .jpg"), 1, 1920, 1080);
                 scene.export3DSceneFileAndWait(resolvePath(dir + sep + scene.getPresentationName() + " .sce"), scene.getPresentationName(), "", false, false);
@@ -45,6 +57,7 @@ public class ExporterV2 extends StarMacro{
             simulation_0.println("Scenes saving complete");
         }
        else {
+		   //if directory exists, do not export
            simulation_0.println("Scenes did not save successfully");
         }
     }
@@ -55,13 +68,14 @@ public class ExporterV2 extends StarMacro{
         File dir = new File(simulationDirectory + sep + "Plots");
 
         if (dir.mkdir()) {
-            //iterate through every plot and export the scenes into a .jpg file
+            //iterate through every plot and export the scenes into a .jpg file and exports data into .csv files
             for (StarPlot plot : simulation_0.getPlotManager().getObjects()) {
                 plot.encode(resolvePath(dir + sep + plot.getPresentationName() + " .jpg"), "jpg", 1920, 1080);
                 plot.export(dir + sep + plot.getPresentationName() + ".csv", ",");
             }
             simulation_0.println("Plot saving complete");
         } else {
+			//if directory already exists, do not export
             simulation_0.println("Plots did not save successfully");
         }
     }
